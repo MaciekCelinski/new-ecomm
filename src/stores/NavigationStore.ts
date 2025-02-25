71e0b0c15244c519a1c0f7d7be835d2ab72dceb7
Maciek <mac.celinski@gmail.com>
2025-02-25 16:33:41 +0100

    user icon dropdown

--- src/stores/NavigationStore.ts
+++ src/stores/NavigationStore.ts
@@ -2,7 +2,7 @@ import { atom, map, deepMap, task } from "nanostores";
 // import type { PreinitializedWritableAtom } from "nanostores";
 
 // export const isMobile: PreinitializedWritableAtom<boolean | null> = atom(null);
-export const isMobile = atom(false);
+export const isMobile = atom(null);
 export const isMobileMenuOpen = atom(false);
 export const navCategories = map([]);
 export const cartProductsCount = atom(0);
